9e076d88eee4c61936c8874fb44ede00a16e5614
mbugua <email@email.com>
2021-03-11 20:21:56 +0300

    more

---
+++ UserApp/src/assets/data/cars.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: '0',
+    type: 'UberX',
+    latitude: -1.231687,
+    longitude: 36.711859,
+    heading: 47,
+  },
+  {
+    id: '1',
+    type: 'Comfort',
+    latitude: 28.456312,
+    longitude: -16.252929,
+    heading: 190,
+  },
+  {
+    id: '2',
+    type: 'UberXL',
+    latitude: -1.231687,
+    longitude: 36.711859,
+    heading: 99,
+  },
+  {
+    id: '3',
+    type: 'Comfort',
+    latitude: 28.454812,
+    longitude:-16.258658,
+    heading: 120,
+  },
+];
